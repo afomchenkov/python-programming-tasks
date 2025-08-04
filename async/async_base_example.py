e241980644d59842b42c4f9f912e9f443a447fd9
Aliaksandr Famchankou <aliaksandr.famchankou@gmail.com>
2025-08-04 15:35:58 +0200

    chore: update base async task

--- async/async_base_example.py
+++ async/async_base_example.py
@@ -1,4 +1,6 @@
 import asyncio
+import threading
+import time
 from random import random
 
 
@@ -8,11 +10,16 @@ async def task(name, delay):
     print(f"{name} finished after {delay} sec")
 
 
+def start_event_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+
 async def main():
     print('before coroutine')
 
     await asyncio.gather(
-        *[task(f'test {i}', random()) for i in range(5)]
+        *[asyncio.create_task(task(f'test {i}', random())) for i in range(5)]
     )
     # await task('test1', 1)
     # await task('test2', 1.1)
@@ -21,5 +28,12 @@ async def main():
 
 
 if __name__ == "__main__":
-    
-    asyncio.run(main())
+    new_loop = asyncio.new_event_loop()
+    t = threading.Thread(target=start_event_loop, args=(new_loop,), daemon=True)
+    t.start()
+
+    asyncio.run_coroutine_threadsafe(main(), new_loop)
+
+    for i in range(5):
+        print(f"[Main Thread] Working... {i}")
+        time.sleep(0.8)
