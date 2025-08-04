b1c2236c3ca0543f65b64a7a5afd80c64a1b95b9
Aliaksandr Famchankou <aliaksandr.famchankou@gmail.com>
2025-08-04 23:23:26 +0200

    feat: add coro multiprocess example

---
+++ concurrency_processes/example/__init__.py
@@ -0,0 +1,4 @@
+from .core import Process, set_context, set_start_method, Worker
+from .pool import Pool, PoolResult
+from .scheduler import RoundRobin, Scheduler
+from .types import QueueID, TaskID
